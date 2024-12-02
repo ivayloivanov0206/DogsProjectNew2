d69bac8503dc41fed7e95c076c6e0bcbef8d8eec
ivayloivanov0206'git config --global user.email i.ivanov2020@pmg-pernik.comgit add .git commit -m Upload <i.ivnaov2020@pmg-pernik.com>
2024-12-02 12:14:51 +0200

    Upload DogsApp

---
+++ Data Access Layer/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DogsApp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Dog>? Dogs { get; set; }
+    }
+}
